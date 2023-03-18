68bbb6868f108c877dfaab1cd5b452be5744920f
MrEgyptian <Me@MrEgyptian.com>
2023-03-18 16:25:35 +0200

    Adding FrontEnd

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Proxy Check</title>
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css?h=e7654e37c177eb8903a0c0a23887fdc5">
+    <link rel="stylesheet" href="/assets/css/Lato.css?h=844c03f5741bd7f7adfc1509591fd284">
+    <link rel="stylesheet" href="/assets/css/Footer-Dark-icons.css?h=befd8a398792e305b7ffd4a176b5b585">
+    <link rel="stylesheet" href="/assets/css/Toggle-Buttons.css?h=326dd3f8ba208152f47ffd2bf0b3c996">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center">Proxy Checker</h1>
+        <p class="text-center fs-4">Check if your proxy works or not</p>
+    </div>
+    <form>
+        <div class="container">
+            <div class="row">
+                <div class="flex-column d-flex col py-4">
+                    <div class="input-group"><input class="form-control" type="text" placeholder="Proxy"><button class="btn btn-primary" type="submit">Check</button></div>
+                </div>
+            </div>
+        </div>
+    </form><!-- Start: Footer Dark -->
+    <footer class="text-center bg-dark">
+        <div class="container text-white py-4 py-lg-5">
+            <ul class="list-inline">
+                <li class="list-inline-item me-4"><a class="link-light" href="https://github.com/MrEgyptian">GitHub</a></li>
+                <li class="list-inline-item me-4"><a class="link-light" href="https://www.MrEgyptian.com">Website</a></li>
+                <li class="list-inline-item"><a class="link-light" href="https://github.com/MrEgyptian/php_proxy_check">Source Code</a></li>
+            </ul>
+            <p class="text-muted mb-0">Copyright © 2023 MrEgyptian</p>
+        </div>
+    </footer><!-- End: Footer Dark -->
+    <script src="/assets/js/jquery.min.js?h=84e399b8f2181ccd73394fdeddff1638"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js?h=79ff9637b74326c362fb6f7f5801a7ba"></script>
+</body>
+
+</html>
